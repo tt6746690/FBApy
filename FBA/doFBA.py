d9a92722d7254fe4739948e02fd52c7824261977
MarkWang <peiqi1122@gmail.com>
2015-06-19 19:58:34 -0400

    did fba on input
    cobrapy doesnt work. have to fix dictioanry key errors

--- FBA/doFBA.py
+++ FBA/doFBA.py
@@ -3,7 +3,6 @@ import os
 
 
 input_file = os.path.join(os.getcwd(), 'input\\', 'Master.json')
-print(input_file)
 '''
 file = open(input_file)
 f = file.read()
@@ -11,11 +10,12 @@ p = f[3230000:3232000]
 print(p)
 print(f[3030105: 3030200])
 '''
-file = open('cobrapyoutputjson.json')
+file = open('target.json')
 f = file.read()
 file.close()
 
 
-model = cobra.io.load_json_model(f)
+model = cobra.io.load_json_model('target.json')
 model.optimize()
 print(model.solution.f)
+
